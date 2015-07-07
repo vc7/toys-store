ad38831070c097bfe9d03d83c9b6a8bc7fabbb59
Vincent Chen <vince78718@gmail.com>
2015-07-07 22:16:32 +0800

    新增欄位後調整 UI 以及 controller - 在所有表格及顯示的畫面加入 amount 欄位 - 在 controller 的 permit 加入 amount ，才有辦法寫入資料庫

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -69,6 +69,6 @@ class ToysController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def toy_params
-      params.require(:toy).permit(:name, :price)
+      params.require(:toy).permit(:name, :price, :amount)
     end
 end
